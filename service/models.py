9dfa35f067f09f7fd51b308a0b063f435f4c7b87
Anton Strokov <anton.strokoff@gmail.com>
2023-05-20 13:12:56 +0300

    command line version temp

--- service/models.py
+++ service/models.py
@@ -1,15 +1,58 @@
 from requests import get
 from service import db
+from datetime import datetime
+from sqlalchemy.exc import IntegrityError, PendingRollbackError
 
 
-class Jserviceapihandler:
+class JServiceApiHandler:
+
+    REQUIRED_COLUMNS = ('id', 'question', 'answer', 'created_at', 'category_id')
 
     def __init__(self, count):
         self.count = count
-        self.__data = get(f"https://jservice.io/api/random?count={count}")
+        self.data = get(f"https://jservice.io/api/random?count={count}")
+        self.ready_data = self.extract_needed_data()
 
-    def json_data(self):
-        return self.__data.json()
+    # def json_data(self):
+    #     return self.data.json()
+
+    def get_question(self):
+        counter = self.count
+        for i in range(counter):
+
+
+    def extract_needed_data(self):
+        temp_list = []
+        for item in self.data.json():
+            question = dict()
+            for key, value in item.items():
+                if key in self.REQUIRED_COLUMNS:
+                    question[key] = value
+            question['created_at'] = self.convert_time(question['created_at'].replace('Z', '').replace('T', ' '))
+            temp_list.append(question)
+        return temp_list
+
+    def commit_to_db(self):
+        for item in self.ready_data:
+            try:
+                question = Question(item['id'], item['question'], item['answer'],
+                                    item['created_at'], item['category_id'])
+                db.session.add(question)
+                db.session.flush()
+                db.session.commit()
+                print('ok')
+
+            except (IntegrityError, PendingRollbackError):
+                print("question exists")
+                obj = JServiceApiHandler(1)
+                obj.commit_to_db()
+                continue
+        return "Succesfull", 200
+
+
+
+    def convert_time(self, time: str):
+        return datetime.strptime(time, '%Y-%m-%d %H:%M:%S.%f')
 
 
 class Question(db.Model):
--- service/routes.py
+++ service/routes.py
@@ -1,29 +1,13 @@
-from service import app, db
+from service import app
 from flask import request
-from .models import Jserviceapihandler, Question
-from datetime import datetime
-#
-# @app.route('/<question_num>', methods=['POST'])
-# def index(question_num):
-#     question_num = question_num
-#     print(question_num)
-#     return question_num
+from .models import JServiceApiHandler
 
 
 @app.route('/count', methods=['GET', 'POST'])
 def count():
-    # title = 'How much questions?'
     questions_num = request.json.get('questions_num')
-    resp = Jserviceapihandler(questions_num)
+    resp = JServiceApiHandler(questions_num)
+    print(resp.ready_data)
+    resp.commit_to_db()
     # print(resp.json_data())
-    for i in resp.json_data():
-        temp = dict()
-        for k, v in i.items():
-            if k in ['id', 'question', 'answer', 'created_at', 'category_id']:
-                temp[k] = v
-        temp['created_at'] = datetime.strptime(temp['created_at'].replace('Z', '').replace('T', ' '), '%Y-%m-%d %H:%M:%S.%f')
-        question = Question(temp['id'], temp['question'], temp['answer'], temp['created_at'], temp['category_id'])
-        db.session.add(question)
-        db.session.commit()
-    print(temp, type(temp['created_at']))
-    return 'Success!', 200
+    return '', 200
