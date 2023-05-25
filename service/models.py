b7bb4e94a6a9939ca59253d3e830480dc8d03b07
Anton Strokov <anton.strokoff@gmail.com>
2023-05-25 11:56:45 +0300

    command line version tmp

--- service/models.py
+++ service/models.py
@@ -1,24 +1,8 @@
 from requests import get
 from service import db
 from datetime import datetime
+from json import dumps
 from sqlalchemy.exc import IntegrityError, PendingRollbackError
-
-
-def check_question_exists(id):
-    if db.session.execute(db.select(Question).filter_by(question_id=id)).scalar():
-        print('True')
-        return True
-    print('False')
-    return False
-
-
-def make_request():
-    resp = get("https://jservice.io/api/random?count=1").json()[0]
-    if check_question_exists(resp.get('id')):
-        return make_request()
-    else:
-        return resp
-
 
 class JServiceApiQuestion:
 
@@ -34,37 +18,21 @@ class JServiceApiQuestion:
         for key, value in self.data.items():
             if key in self.REQUIRED_COLUMNS:
                 d.setdefault(key, value)
-        d['created_at'] = self.convert_time(d['created_at'].replace('Z', '').replace('T', ' '))
+        d['created_at'] = datetime.strptime(d['created_at'].replace('Z', '').replace('T', ' '), self.TIME_FORMAT)
         return d
 
     def commit_to_db(self):
-        question = Question(self.processed_data.get('id'), self.processed_data.get('question'),
-                            self.processed_data.get('answer'), self.processed_data.get('created_at'),
+        question = Question(self.processed_data.get('id'),
+                            self.processed_data.get('question'),
+                            self.processed_data.get('answer'),
+                            self.processed_data.get('created_at'),
                             self.processed_data.get('category_id'))
         db.session.add(question)
         db.session.flush()
         db.session.commit()
-        # except (IntegrityError, PendingRollbackError):
-        return "Successful", 200
-
-    def convert_time(self, time: str):
-        return datetime.strptime(time, self.TIME_FORMAT)
 
     def __repr__(self):
         return f'{self.processed_data.get("id")}: {self.processed_data.get("question")}'
-
-
-
-#
-#
-# def get_question(count):
-#     for i in range(count):
-#         a = JServiceApiQuestion()
-#         if check_db(a.processed_data.get('id')):
-#             a = JServiceApiQuestion()
-#             a.commit_to_db()
-#         else:
-#             a.commit_to_db()
 
 
 class Question(db.Model):
@@ -86,3 +54,26 @@ class Question(db.Model):
 
     def __repr__(self):
         return f'{self.question_id}: {self.question_text}'
+
+    def as_json(self):
+        return dumps({
+            'id': self.id,
+            'question_id': self.question_id,
+            'question': self.question_text,
+            'answer': self.question_answer,
+            'category_id': self.category_id,
+            'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S.%f')
+        })
+
+
+def get_last_question():
+    return db.session.query(Question).order_by(Question.id.desc()).first().as_json()
+
+
+def check_question_exists(id):
+    return db.session.execute(db.select(Question).filter_by(question_id=id)).scalar()
+
+
+def make_request():
+    resp = get("https://jservice.io/api/random?count=1").json()[0]
+    return make_request() if check_question_exists(resp.get('id')) else resp
--- service/routes.py
+++ service/routes.py
@@ -1,39 +1,30 @@
 from service import app
 from flask import request
 from requests import get
-from .models import JServiceApiQuestion, make_request, check_question_exists
+from json import dumps
+from .models import JServiceApiQuestion, make_request, check_question_exists, get_last_question
 from sqlalchemy import select
 from requests.exceptions import JSONDecodeError
 
 
 @app.route('/count', methods=['GET', 'POST'])
 def count():
+    last_question = get_last_question()
     questions_num = request.json.get('questions_num')
-    req = get(f"https://jservice.io/api/random?count={questions_num}").json()
-    for item in req:
-        a = JServiceApiQuestion(item)
-        if not check_question_exists(a.processed_data.get('id')):
-            a.commit_to_db()
-        else:
-            print('make req activated')
-            b = JServiceApiQuestion(make_request())
-            b.commit_to_db()
+    if questions_num < 101:
+        req = get(f"https://jservice.io/api/random?count={questions_num}").json()
+        for item in req:
+            a = JServiceApiQuestion(item)
+            if not check_question_exists(a.processed_data.get('id')):
+                a.commit_to_db()
+            else:
+                b = JServiceApiQuestion(make_request())
+                b.commit_to_db()
+        return last_question
+    else:
+        return "Too many questions requested, questions_num should be <= 100", 405
 
-
-    # questions = [item[0] for item in db.session.execute(select(Question.question_id)).all()]
-    # print(questions)
-    # questions_num = request.json.get('questions_num')
-    # while questions_num > 0:
-    #     # try:
-    #     a = JServiceApiQuestion()
-    #     if a.processed_data.get('id') in questions:
-    #         questions_num += 1
-    #     else:
-    #         a.commit_to_db()
-    #         questions_num -= 1
-        # except JSONDecodeError:
-        #
-        #     print(a.data, a.processed_data)
-        #     print('WHAT THE FUCK')
-        #     continue
-    return '', 200
+#
+# @app.route('/last_question')
+# def last_question():
+#     return get_last_question(), 200
