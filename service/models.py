67726467ba0a5b36ef6f6ea5afab1ee2104cde64
Anton Strokov <anton.strokoff@gmail.com>
2023-05-25 14:57:39 +0300

    command line version tmp

--- service/models.py
+++ service/models.py
@@ -2,31 +2,43 @@ from requests import get
 from service import db
 from datetime import datetime
 from json import dumps
-from sqlalchemy.exc import IntegrityError, PendingRollbackError
+
 
 class JServiceApiQuestion:
 
-    REQUIRED_COLUMNS = ('id', 'question', 'answer', 'created_at', 'category_id')
-    TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+    REQUIRED_COLUMNS: tuple[str] = ('id', 'question', 'answer', 'created_at', 'category_id')
+    TIME_FORMAT: str = '%Y-%m-%d %H:%M:%S.%f'
 
-    def __init__(self, req):
-        self.data = req
-        self.processed_data = self.extract_needed_data()
+    def __init__(self, resp: dict) -> None:
+        self.data: dict[str, str | int| dict] = resp
+        self.processed_data: dict[str, str | int | datetime] = self.extract_needed_data()
 
-    def extract_needed_data(self):
-        d = {}
+    def extract_needed_data(self) -> dict[str, str | int | datetime]:
+        """
+        Метод выделя
+        :return:
+        """
+
+        temp_dict: dict[str, str | int | datetime] = dict()
         for key, value in self.data.items():
             if key in self.REQUIRED_COLUMNS:
-                d.setdefault(key, value)
-        d['created_at'] = datetime.strptime(d['created_at'].replace('Z', '').replace('T', ' '), self.TIME_FORMAT)
-        return d
+                temp_dict.setdefault(key, value)
+        temp_dict['created_at'] = datetime.strptime(temp_dict['created_at'].replace('Z', '').replace('T', ' '),
+                                                    self.TIME_FORMAT)
+        return temp_dict
 
-    def commit_to_db(self):
-        question = Question(self.processed_data.get('id'),
-                            self.processed_data.get('question'),
-                            self.processed_data.get('answer'),
-                            self.processed_data.get('created_at'),
-                            self.processed_data.get('category_id'))
+    def commit_to_db(self) -> None:
+        """
+        Метод создает экземпляр класса Question
+        и транслирует его в базу данных, используя
+        методы SQLAlchemy
+        :return: None
+        """
+        question: Question = Question(self.processed_data.get('id'),
+                                      self.processed_data.get('question'),
+                                      self.processed_data.get('answer'),
+                                      self.processed_data.get('created_at'),
+                                      self.processed_data.get('category_id'))
         db.session.add(question)
         db.session.flush()
         db.session.commit()
@@ -38,21 +50,25 @@ class JServiceApiQuestion:
 class Question(db.Model):
     __tablename__ = "questions"
 
-    id = db.Column(db.Integer(), nullable=False, primary_key=True)
-    question_id = db.Column(db.Integer(), nullable=False, unique=True)
-    question_text = db.Column(db.String(100), nullable=False)
-    question_answer = db.Column(db.String(30), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    category_id = db.Column(db.Integer(), nullable=False)
+    id: int = db.Column(db.Integer(), nullable=False, primary_key=True)
+    question_id: int = db.Column(db.Integer(), nullable=False, unique=True)
+    question_text: str = db.Column(db.String(100), nullable=False)
+    question_answer: str = db.Column(db.String(30), nullable=False)
+    created_at: datetime = db.Column(db.DateTime, nullable=False)
+    category_id: int = db.Column(db.Integer(), nullable=False)
 
-    def __init__(self, question_id, question_text, question_answer, created_at, category_id):
+    def __init__(self, question_id: int,
+                 question_text: str,
+                 question_answer: str,
+                 created_at: datetime,
+                 category_id: int) -> None:
         self.question_id = question_id
         self.question_text = question_text
         self.question_answer = question_answer
         self.created_at = created_at
         self.category_id = category_id
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'{self.question_id}: {self.question_text}'
 
     def as_json(self):
@@ -62,18 +78,32 @@ class Question(db.Model):
             'question': self.question_text,
             'answer': self.question_answer,
             'category_id': self.category_id,
-            'created_at': self.created_at.strftime('%Y-%m-%d %H:%M:%S.%f')
+            'created_at': self.created_at.strftime(JServiceApiQuestion.TIME_FORMAT)
         })
 
 
-def get_last_question():
-    return db.session.query(Question).order_by(Question.id.desc()).first().as_json()
+def get_last_question() -> str:
+    try:
+        last_q: str = db.session.query(Question).order_by(Question.id.desc()).first().as_json()
+        return last_q
+    except AttributeError:
+        return " "
 
 
-def check_question_exists(id):
-    return db.session.execute(db.select(Question).filter_by(question_id=id)).scalar()
+def check_question_exists(question_id: int) -> Question | None:
+    """
+
+    :param question_id:
+    :return:
+    """
+    return db.session.execute(db.select(Question).filter_by(question_id=question_id)).scalar()
 
 
-def make_request():
+def make_request() -> dict:
+    """
+    Функция делает единичный запрос к API jservice
+    и возвращает словарь с результатом
+    :return: str
+    """
     resp = get("https://jservice.io/api/random?count=1").json()[0]
     return make_request() if check_question_exists(resp.get('id')) else resp
--- service/routes.py
+++ service/routes.py
@@ -1,10 +1,7 @@
 from service import app
 from flask import request
 from requests import get
-from json import dumps
 from .models import JServiceApiQuestion, make_request, check_question_exists, get_last_question
-from sqlalchemy import select
-from requests.exceptions import JSONDecodeError
 
 
 @app.route('/count', methods=['GET', 'POST'])
@@ -12,10 +9,11 @@ def count():
     last_question = get_last_question()
     questions_num = request.json.get('questions_num')
     if questions_num < 101:
-        req = get(f"https://jservice.io/api/random?count={questions_num}").json()
+        req: list = get(f"https://jservice.io/api/random?count={questions_num}").json()
         for item in req:
             a = JServiceApiQuestion(item)
             if not check_question_exists(a.processed_data.get('id')):
+                print(check_question_exists(a.processed_data.get('id')))
                 a.commit_to_db()
             else:
                 b = JServiceApiQuestion(make_request())
@@ -23,8 +21,3 @@ def count():
         return last_question
     else:
         return "Too many questions requested, questions_num should be <= 100", 405
-
-#
-# @app.route('/last_question')
-# def last_question():
-#     return get_last_question(), 200
