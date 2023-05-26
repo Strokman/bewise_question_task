7965f4d7bb64871223bff2e5fa47ab9c2e7718dc
Anton Strokov <anton.strokoff@gmail.com>
2023-05-26 17:08:32 +0300

    gunicorn

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True, host='0.0.0.0')
+else:
+    with app.app_context():
+        db.create_all()
+    app.app_context().push()
+
 
