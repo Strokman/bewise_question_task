cd158db33362742c564fdd6d0310a8f8cf61dbfd
Anton Strokov <anton.strokoff@gmail.com>
2023-05-28 15:28:50 +0300

    v. 1.0

--- questions/routes.py
+++ questions/routes.py
@@ -3,7 +3,6 @@ from flask import request
 from requests import get
 from .helpers import make_request, check_question_exists, get_last_question
 from .models import JServiceApiQuestion
-from json.decoder import JSONDecodeError
 
 
 @app.route('/count', methods=['POST'])
@@ -16,20 +15,16 @@ def count():
     :return:
     """
     last_question: str = get_last_question()
-    try:
-        questions_num: int = request.json.get('questions_num')
-    except JSONDecodeError:
-        return 'Please provide correct request body with json-string', 400
-    finally:
-        if 0 < questions_num < 101:
-            resp: list = get(f"https://jservice.io/api/random?count={questions_num}").json()
-            for item in resp:
-                question = JServiceApiQuestion(item)
-                if not check_question_exists(question.processed_data.get('id')):
-                    question.commit_to_db()
-                else:
-                    another_question = JServiceApiQuestion(make_request())
-                    another_question.commit_to_db()
-            return last_question
-        else:
-            return "Please provide correct number of questions: 0 < questions_num < 100", 405
+    questions_num: int = request.json.get('questions_num')
+    if 0 < questions_num < 101:
+        resp: list = get(f"https://jservice.io/api/random?count={questions_num}").json()
+        for item in resp:
+            question = JServiceApiQuestion(item)
+            if not check_question_exists(question.processed_data.get('id')):
+                question.commit_to_db()
+            else:
+                another_question = JServiceApiQuestion(make_request())
+                another_question.commit_to_db()
+        return last_question
+    else:
+        return "Please provide correct number of questions: 0 < questions_num < 100", 405
